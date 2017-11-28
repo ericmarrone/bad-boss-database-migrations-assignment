a325d4a50eb973072ba3041df83920fa5c1490f8
Eric Marrone <emarrone17@gmail.com>
2017-11-28 01:51:09 -0500

    Add locations migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128063922) do
+ActiveRecord::Schema.define(version: 20171128065028) do
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.string "region"
+  end
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
